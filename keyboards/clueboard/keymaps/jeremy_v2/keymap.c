c7ee93c7f542747b952e6759de9355365520a741
Jeremy Cowgar <jeremy@cowgar.com>
2017-09-19 00:19:57 -0400

    Added layouts for Tarmak, Colemak and Dvorak as well as the Angle/Wide Ergo Mod

--- keyboards/clueboard/keymaps/jeremy_v2/keymap.c
+++ keyboards/clueboard/keymaps/jeremy_v2/keymap.c
@@ -1,11 +1,15 @@
 #include "clueboard.h"
 
 enum {
-  ALPH = 0, // Alpha
-  NUMS,     // Numbers
-  CODE,     // Coding (special characters)
+  QWER = 0, // Qwerty
+  TAR1,     // Tarmak 1
+  TAR2,     // Tarmak 2
+  TAR3,     // Tarmak 3
+  TAR4,     // Tarmak 4
+  COLM,     // Colemak
+  DVOR,     // Dvorak
   CURS,     // Cursor Navigation
-  FKEY      // Function Keys
+  CNFG      // Config
 };
 
 enum {
@@ -20,6 +24,15 @@ enum {
   M_BRSWP,              // Braces Toggle [] vs. {}
   MC_LBRC,
   MC_RBRC,
+  M_CTRSF,
+  M_ALTSF,
+  DL_QWER,
+  DL_TAR1,
+  DL_TAR2,
+  DL_TAR3,
+  DL_TAR4,
+  DL_COLM,
+  DL_DVOR,
   DYNAMIC_MACRO_RANGE
 };
 
@@ -50,37 +63,144 @@ enum {
 #define DYNM_ST DYN_REC_STOP
 
 #define LT_CURS LT(CURS, KC_SPC)
+#define LT_CNFG MO(CNFG)
+
+#define BM_SGE MAGIC_SWAP_GRAVE_ESC
+#define BM_UGE MAGIC_UNSWAP_GRAVE_ESC
+#define BM_SBB MAGIC_SWAP_BACKSLASH_BACKSPACE
+#define BM_UBB MAGIC_UNSWAP_BACKSLASH_BACKSPACE
 
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 #define X       KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[ALPH] = KEYMAP(
+[QWER] = KEYMAP(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DELT, M_NMTGL,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          M_BRSWP,
   KC_LEAD, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, X,       KC_ENT,
-  KC_LSFT, X,KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, M_SFLK,  KC_UP,
-  KC_ESC,  KC_LGUI, KC_LCTL, KC_LALT, LT_CURS, LT_CURS,                   KC_RALT, KC_RCTL, KC_MENU, KC_ESC,  KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, M_SFLK,  KC_UP,
+  M_CTRSF, KC_LGUI, KC_LCTL, KC_LALT, LT_CURS, LT_CURS,                   KC_RALT, KC_RCTL, KC_MENU, M_ALTSF, KC_LEFT, KC_DOWN, KC_RGHT),
+[TAR1] = KEYMAP(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, KC_J,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_N,    KC_E,    _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_K,    _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______),
+[TAR2] = KEYMAP(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, KC_F,    _______, KC_G,    _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+  _______, _______, _______, _______, KC_T,    KC_J,    _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______),
+[TAR3] = KEYMAP(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, KC_J,    _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+  _______, _______, KC_R,    KC_S,    _______, KC_D,    _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______),
+[TAR4] = KEYMAP(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, KC_P,    _______, KC_J,    _______, _______, KC_Y,    KC_SCLN, _______, _______, _______,          _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_O,    _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______),
+[COLM] = KEYMAP(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_L,    KC_U,    _______, _______, _______, _______, _______,          _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_I,    _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______),
+[DVOR] = KEYMAP(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______,
+  _______, KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_PLUS, KC_BSLS,          _______,
+  _______, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, _______, _______,
+  _______, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_ESC,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______),
 [CURS] = KEYMAP(
   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F13,  KC_F14,  RESET,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_Y,    KC_HOME, KC_UP,   KC_END,  KC_COPY, MC_LBRC, MC_RBRC, XXXXXXX,          XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_Y,    KC_HOME, KC_UP,   KC_END,  KC_COPY, MC_LBRC, MC_RBRC, XXXXXXX,          LT_CNFG,
   _______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, KC_CUT,  XXXXXXX, XXXXXXX, _______,
-  _______, X,KC_NO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, KC_PASTE,XXXXXXX, _______, _______,
-  _______, _______, _______, AG_NORM, _______, _______,                   AG_SWAP, _______, _______, _______, _______, _______, _______)
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX, KC_PASTE,XXXXXXX, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______),
+[CNFG] = KEYMAP(
+  BM_SGE,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, BM_UBB,  BM_UBB,  XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, BM_SBB,           XXXXXXX,
+  XXXXXXX, DL_QWER, DL_TAR1, DL_TAR2, DL_TAR3, DL_TAR4, DL_COLM, DL_DVOR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, BM_UGE,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, AG_NORM, _______, XXXXXXX,                   AG_SWAP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX),
 };
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
 
 bool shift_lock = false;
 bool number_toggle = false;
 bool brace_toggle = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  static uint16_t mods;
+
   if (!process_record_dynamic_macro(keycode, record)) {
     return false;
   }
 
   if (record->event.pressed) {
+    if (keycode == KC_ENT) {
+      mods = get_mods();
+
+      if (mods & (MOD_BIT(KC_LCTL)) && mods & (MOD_BIT(KC_LSFT))) {
+        keycode = M_ENTCM;
+      } else if (mods & (MOD_BIT(KC_LCTL)|MOD_BIT(KC_RCTL))) {
+        keycode = M_ENTUP;
+      } else if (mods & (MOD_BIT(KC_LALT)|MOD_BIT(KC_RALT))) {
+        keycode = M_ENTDN;
+      } else if (mods & (MOD_BIT(KC_LSFT)|MOD_BIT(KC_RSFT))) {
+        keycode = M_ENTBR;
+      }
+    }
+
     switch (keycode) {
+      case DL_QWER:
+        persistent_default_layer_set(1UL<<QWER);
+        return false;
+
+      case DL_TAR1:
+        persistent_default_layer_set(1UL<<TAR1);
+        return false;
+
+      case DL_TAR2:
+        persistent_default_layer_set(1UL<<TAR2);
+        return false;
+
+      case DL_TAR3:
+        persistent_default_layer_set(1UL<<TAR3);
+        return false;
+
+      case DL_TAR4:
+        persistent_default_layer_set(1UL<<TAR4);
+        return false;
+
+      case DL_COLM:
+        persistent_default_layer_set(1UL<<COLM);
+        return false;
+
+      case DL_DVOR:
+        persistent_default_layer_set(1UL<<DVOR);
+        return false;
+
+      case M_CTRSF:
+        register_code(KC_LCTL);
+        register_code(KC_LSFT);
+        return false;
+
+      case M_ALTSF:
+        register_code(KC_LALT);
+        register_code(KC_LSFT);
+        return false;
+
       case M_ENTUP:
         SEND_STRING(SS_TAP(X_HOME)"\n"SS_TAP(X_UP));
         return false;
@@ -155,6 +275,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
       case KC_LBRC:
       case KC_RBRC:
+      case KC_BSLS:
         if (brace_toggle) {
           uint16_t mod_code = KC_NO;
 
@@ -197,6 +318,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
     }
   } else {
+    switch (keycode) {
+      case M_CTRSF:
+        unregister_code(KC_LSFT);
+        unregister_code(KC_LCTL);
+        return false;
+
+      case M_ALTSF:
+        unregister_code(KC_LSFT);
+        unregister_code(KC_LALT);
+        return false;
+    }
+
     if (shift_lock) {
       unregister_code(KC_LSFT);
     }
